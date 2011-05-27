9b53bb148f4ce439243de0dbcf15f0a3c5c742c8
Tim Nagel <tim@nagel.com.au>
2011-05-27 12:02:08 +1000

    Updates to FOS, added Test bundle

---
+++ src/Test/AppBundle/Entity/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Test\AppBundle\Entity;
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\generatedValue(strategy="AUTO")
+     */
+    protected $id;
+}
---
+++ src/Test/AppBundle/TestAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Test\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestAppBundle extends Bundle
+{
+}
