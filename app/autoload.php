65e0f982fb2eaf08a9ceaf9d45cd2852ceeca5c2
Tim Nagel <tim@nagel.com.au>
2011-07-14 19:29:16 +1000

    Autoload changes, deps additions

--- app/autoload.php
+++ app/autoload.php
@@ -16,6 +16,7 @@ $loader->registerNamespaces(array(
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'merk'             => __DIR__.'/../vendor/bundles',
+    'Ornicar'          => __DIR__.'/../vendor/bundles',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
